7af0351fd95730f2744a44114b65ca0ccd043a93
CodigoDeSenior <migueljnm840@gmail.com>
2022-10-28 09:55:57 -0300

    Added PUT route and tests for it

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,27 @@ const express = require('express');
 const createError = require('http-errors');
 const router = express.Router();
 
-const items = [{id: 1, name: "Grass", tier: 1, stars: 2}];
+const items = [{id: 0, name: "Grass", tier: 1, stars: 2}];
 
 router.get('/', (req, res) => {
     res.json(items);
 });
 
-router.post('/', (req, res, next) => {
+router.get('/:id', (req, res) => {
+    const item = items[req.params.id];
+
+    if(!item) {	
+	res.status(404).send({ error: 'Item Not Found', statusCode: 404});
+    }
+
+    res.json(item);
+});
+
+router.post('/', (req, res) => {
     const { body } = req;
 
     if( typeof body.name !== 'string') {
-	return next(createError(400, 'Validation Error(name != string)'));
+	res.status(400).send({ error: 'Bad Request(name == "string")', statusCode: 404});
     }
 
     const newItem = {
@@ -24,6 +34,26 @@ router.post('/', (req, res, next) => {
 
     items.push(newItem);
     res.status(201).json(newItem);
+});
+
+router.put('/:id', (req, res) => {
+    const { body } = req;
+
+    if ( body.name && typeof body.name !== 'string' ) {
+	res.status(400).send({ error: 'Bad Request(name == "string")' });
+    }
+
+    const oldItem = items[req.params.id];
+    if(!oldItem) {
+	res.status(404).send({ error: 'Item Not Found', statusCode: 404});
+    }
+    const newItem = {...oldItem, ...body};
+    
+    items[req.params.id] = newItem;
+
+    res.status(201).json(newItem);
+
+
 });
 
 module.exports = router;
--- routes/test/index.test.js
+++ routes/test/index.test.js
@@ -23,7 +23,30 @@ describe('REST /items', () => {
 	    });
     });
 
-    it('POST /items => create NEW item', () => {
+    it('GET /items/:id => one item', () => {
+	return request(app)
+	    .get("/items/0")
+	    .expect('Content-Type', /json/)
+	    .expect(200)
+	    .then((response) => {
+		expect(response.body).toEqual(
+		    expect.objectContaining({
+			id: expect.any(Number),
+			name: expect.any(String),
+			tier: expect.any(Number),
+			stars: expect.any(Number),
+		    })
+		);
+	    });
+    });
+
+    it('GET /items/:id => not found', () => {
+	return request(app)
+	    .get("/items/1000000000")
+	    .expect(404);
+    });
+
+    it('POST /items => create new item', () => {
 	return request(app)
 	    .post('/items')
 	    .send({name: "Heavenly Iron", tier: 9, stars: 8 })
@@ -39,11 +62,49 @@ describe('REST /items', () => {
 	    })
     });
 
-    it('POST /items => NAME type check', () => {
+    it('POST /items => type check(FAIL)', () => {
 	return request(app)
 	    .post('/items')
 	    .send({ name: 123444, tier: 3, stars: 6})
 	    .expect(400);
+    });
+
+    it('PUT /items/:id => update one field', () => {
+	return request(app)
+	    .put('/items/0')
+	    .send( { name: "Cold Heavenly Iron" } )
+	    .expect(201)
+	    .then((response) => {
+		expect(response.body).toEqual(
+		    expect.objectContaining({
+			name: "Cold Heavenly Iron",
+			tier: expect.any(Number),
+			stars: expect.any(Number),
+		    })
+		);
+	    });
+    });
+    
+    it('PUT /items/:id => update (NOT FOUND)', () => {
+	return request(app)
+	    .put('/items/1000000000')
+	    .expect(404);
+    });
+
+    it('PUT /items/:id => update multiple fields', () => {
+	return request(app)
+	    .put('/items/0')
+	    .send( { name: "Cold Heavenly Iron", tier: 9, stars: 7 } )
+	    .expect(201)
+	    .then((response) => {
+		expect(response.body).toEqual(
+		    expect.objectContaining({
+			name: "Cold Heavenly Iron",
+			tier: 9,
+			stars: 7, 
+		    })
+		);
+	    });
     });
 
 });
